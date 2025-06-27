84f1afd55ff142766b9fe013bf874478b1440560
gaa <gaa@wildbox.ru>
2025-06-28 02:05:57 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('API is running 🚀')
+})
+
+app.use('/api/auth', authRoutes)
+
+export default app
